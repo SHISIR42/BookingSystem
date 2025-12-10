d765f02e3a3a5bd330b4cf014843b31f0dd7ee59
SHISIR42 <SHISIR42@users.noreply.github.com>
2025-12-10 01:50:27 +0000

    Fix test imports for Spring Boot compatibility

--- src/test/java/com/swe7303/devops/controller/UserControllerTest.java
+++ src/test/java/com/swe7303/devops/controller/UserControllerTest.java
@@ -4,8 +4,9 @@ import com.swe7303.devops.model.User;
 import com.swe7303.devops.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
