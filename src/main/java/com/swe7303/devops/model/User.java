72bea900c95c4480e5bf7b0d22e5d7d139c7a5b3
SHISIR42 <SHISIR42@users.noreply.github.com>
2025-12-10 12:12:46 +0000

    Escape user table name for PostgreSQL compatibility

--- src/main/java/com/swe7303/devops/model/User.java
+++ src/main/java/com/swe7303/devops/model/User.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "User")
+@Table(name = "\"user\"")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
